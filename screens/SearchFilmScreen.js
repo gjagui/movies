cc05a2fa267b3aca49022267b6853313d58bfc71
Gustavo Javier Aguirre <gjagui@gmail.com>
2019-04-25 10:38:00 -0300

    Agregado: Fetch de Peliculas y Render de Posters, Title y Year.

--- screens/SearchFilmScreen.js
+++ screens/SearchFilmScreen.js
@@ -1,98 +1,82 @@
-import { SearchBar } from 'react-native-elements';
-import React, { Component } from 'react';
-import {
-    View,
-    Button,
-    TextInput,
-    StyleSheet,
-    Image
-} from 'react-native'
+import React, { Component }               from 'react';
+import { ScrollView, View, Text, TouchableHighlight, Image, StyleSheet } from 'react-native';
+import { SearchBar }                      from 'react-native-elements';
 
-export default class SearchFilm extends React.Component {
-    static navigationOptions = {
-        title: 'Películas a calificar',
-    };
+const MOVIE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=d0b64143&s=";
 
-    state = {
-        search: '',
-    }
-
-    updateSearch = search => {
-      this.setState({ search });
-    };
+export default class SearchFilm extends Component {
   
-    render() {
-      const { search } = this.state;
-      
-      return (
-        
-        <SearchBar
-          placeholder="¿Qué película te gustaría calificar?"
-          onChangeText={this.updateSearch}
-          value={search}  
-          lightTheme={true}        
+  constructor(props) {
+    super(props);
+    this.state = { 
+      search: "",
+      movies: []
+    };
+  }
+    
+  static navigationOptions = { title: 'Películas a calificar' };
+
+  updateSearch = search => {
+
+    let movies = [];
+
+    if (search.length > 4) {
+
+      fetch(`${MOVIE_URL}${search}`)
+      .then(result => result.json())
+      .then(movies_json => {
+        if (movies_json.Response == "True") {
+          movies_json.Search.forEach(movie_value => {
+            let {Poster, Title, Year} = movie_value;
+            let movie = {Poster, Title, Year};
+            movies.push(movie);
+          });
+          this.setState({ movies });
+        }
+      })
+      .catch(error => console.log(error));
+    }
+    this.setState({ search });
+  }
+  
+  render() {
+    
+    let cards = <Text></Text>;
+
+    if (this.state.movies.length > 0) {
+      cards = this.state.movies.map((movie) => {
+        return (
+            <View style={ styles.card }>
+            <Image key = {movie.Poster}
+                  source={{uri: movie.Poster}}
+                  style={{width: 100, height: 100}} />
+            <TouchableHighlight onPress={() => this._onPressButton}>
+            <Text key = {movie.Title}>
+            {movie.Title}
+            </Text>
+            </TouchableHighlight>
+            </View>
+        );
+      });  
+    }
+    
+    return (
+      <View>
+      <SearchBar
+        placeholder = "¿Qué película te gustaría calificar?"
+        onChangeText = { this.updateSearch }
+        value = { this.state.search }  
+        lightTheme ={ true }
         />
+        <ScrollView>{ cards }</ScrollView>
+      </View>
       );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      flex: 1
-    },
-    headerText: {
-      fontSize: 20,
-      textAlign: "center",
-      margin: 10,
-      fontWeight: "bold"
-    },
-    item: {
-      color: 'white',
-      backgroundColor: "#b478ed",
-      fontSize: 20,
-      marginTop: 3,
-      padding:10,
-    },
-  textInput: {
-      height: 30,
-      borderWidth: 1,
-      borderColor: '#cecece',
-      marginBottom: 10,
-      marginHorizontal: 10
-  },
-  content: {
-      zIndex: 1
-  },
-  footerContainer: {
-     flexDirection: 'row',
-     paddingHorizontal: 10,
-     paddingVertical: 10,
-     backgroundColor: '#555566' 
-  },
-  imageAvatar: {
-      width: 50,
-      height: 50,
-      borderRadius: 25,
-      marginRight: 5
-  },
-  listContainer: {
-      marginHorizontal: 10
-  },
-  text: {
-      color: '#fff'
-  },
-  containerCell: {
-      marginBottom: 10
-  },
-  textTitle: {
-      fontSize: 13
-  },
-  textBy: {
-      fontSize: 12
-  },
-  textMenu: {
-      fontSize: 20,
-      color: '#fff'
+  card: {
+    flexDirection: 'column',
+    alignItems: 'center'
   }
-  });
+});
