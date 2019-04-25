e393ffda60786c7ce6d54865d4d75a57193379a7
Danila <danijosevich@gmail.com>
2019-04-25 15:20:52 -0300

    boton para subir comentarios

--- screens/RatingScreen.js
+++ screens/RatingScreen.js
@@ -6,8 +6,8 @@ import {
   StyleSheet,
   ImageBackground,
   Text,
-  Image,
-  ScrollView
+  ScrollView,
+  Button
 } from 'react-native'
 
 const MOVIE_URL = "http://www.omdbapi.com/?apikey=d0b64143&i=";
@@ -23,10 +23,11 @@ export default class RatingScreen extends Component {
       comment_rating: 0,
       rating: 0,
       comments: [],
-      votes: 0
+      votes: 0,
+      comentario: "",
     }
   }
-  
+
   componentWillMount = () => {
     this.fetchComments("tt0120338");
     this.fetchMovies("tt0120338");
@@ -34,21 +35,27 @@ export default class RatingScreen extends Component {
 
   fetchMovies = imdbID => {
     fetch(`${MOVIE_URL}${imdbID}`)
-    .then(result => result.json())
-    .then(movie => { this.setState({ movie })})
-    .catch(error => console.log(error));
+      .then(result => result.json())
+      .then(movie => { this.setState({ movie }) })
+      .catch(error => console.log(error));
   }
-    
+
   fetchComments = (imdbID = "tt0120338") => {
     fetch(`${COMMENTS_URL}${imdbID}`)
-    .then(result => result.json())
-    .then(comments => { this.setState({ comments: comments.comments, rating: comments.rating, votes: comments.votes })})
-    .catch(error => console.log(error));
+      .then(result => result.json())
+      .then(comments => { this.setState({ comments: comments.comments, rating: comments.rating, votes: comments.votes }) })
+      .catch(error => console.log(error));
   }
 
   onChangeText = (key, val) => this.setState({ [key]: val });
 
-  ratingCompleted = (rating) => this.setState({comment_rating : rating});
+  ratingCompleted = (rating) => this.setState({ comment_rating: rating });
+
+  guardarCalificacion() {
+    console.log("Entre");
+    console.log("Rating: " + this.state.rating);
+    console.log("Comentario: " + this.state.comentario);
+  }
 
   render() {
 
@@ -57,70 +64,75 @@ export default class RatingScreen extends Component {
     if (this.state.comments.length > 0) {
 
       comments = this.state.comments.map((comments) => {
-        
-        return (
-            <View key= {comments._id}>
 
-              <Text>
-                Usuario: {comments.username}
-                Voto: {comments.votes}
-                Comentario: {comments.comment}
-              </Text>
-            
-            </View>
+        return (
+          <View key={comments._id}>
+
+            <Text>
+              Usuario: {comments.username}
+              Voto: {comments.votes}
+              Comentario: {comments.comment}
+            </Text>
+
+          </View>
         );
-      });  
+      });
     }
 
-        return (
-          <ScrollView>
+    return (
+      <ScrollView>
 
-          <ImageBackground source={{uri: this.state.movie.Poster}} style={{width: '100%', height: '100%'}}>
-            <Text>Titulo: {this.state.title} Rating: {this.state.rating} Votos: {this.state.votes}</Text>
+        <ImageBackground source={{ uri: this.state.movie.Poster }} style={{ width: '100%', height: '100%' }}>
+          <Text>Titulo: {this.state.title} Rating: {this.state.rating} Votos: {this.state.votes}</Text>
 
-            {comments}
-                          
-            <TextInput
+          {comments}
+
+          <TextInput
             style={styles.input}
             placeholder='Escribe un comentario ...'
             textAlignVertical='top'
-            alignItems= "center"
+            alignItems="center"
             autoCapitalize="none"
             placeholderTextColor='gray'
-            onChangeText={val => this.onChangeText('comentario', val)}/>
+            onChangeText={val => this.onChangeText('comentario', val)} />
 
-            <Rating 
-              showRating
-              onFinishRating={this.ratingCompleted.bind(this)}
-              style={{ paddingVertical: 10 }}
-              />
-            
-          </ImageBackground>
-          
-          </ScrollView>
-       );
-    }
+          <Rating
+            showRating
+            onFinishRating={this.ratingCompleted.bind(this)}
+            style={{ paddingVertical: 10 }}
+          />
+
+          <Button
+            title='Calificar'
+            onPress={this.guardarCalificacion.bind(this)}
+            borderRadius={10}
+          />
+        </ImageBackground>
+
+      </ScrollView>
+    );
+  }
 
 }
 
 const styles = StyleSheet.create({
-    input: {
-      width: 390,
-      height: 250,
-      backgroundColor: 'white',
-      margin: 10,
-      padding: 8,
-      color: 'black',
-      borderRadius: 14,
-      fontSize: 18,
-      fontWeight: '200',
-    },
+  input: {
+    width: 390,
+    height: 250,
+    backgroundColor: 'white',
+    margin: 10,
+    padding: 8,
+    color: 'black',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '200',
+  },
 
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
 
 
