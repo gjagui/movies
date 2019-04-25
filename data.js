ccc7b7679be077e735d43ffaa335d7e83149b290
JP-PC\Juan <jpga87@gmail.com>
2019-04-25 11:40:44 -0300

    Nueva imagen de App

--- data.js
+++ data.js
@@ -4,7 +4,6 @@ export default data = [
         image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/stpatricks-soft-pretzel.jpg',
         food: 'Cinnamon sugar soft pretzel',
         title: 'Perfect soft pretzels',
-        user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
         by: 'Kimberly',
     },
     {
@@ -12,7 +11,6 @@ export default data = [
         image: 'https://www.fivelittlechefs.com/wp-content/uploads/2012/02/strawberry-rocky-road-recipe.jpg',
         food: 'Strawberry Rocky Road',
         title: 'Awesome strawberry aww',
-        user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
         by: 'Kimberly',
     },
     {
@@ -20,7 +18,6 @@ export default data = [
         image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/valentine-coconut-macaroons.jpg',
         food: 'Coconut Macaroons',
         title: 'Coconut macaroons is truly an easy recipe',
-        user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
         by: 'Kimberly',
     }
 ]
